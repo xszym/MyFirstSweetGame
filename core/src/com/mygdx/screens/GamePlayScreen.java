857ff93971b1eaa5ca0e64ddfe2d94b5bbbdc46f
DESKTOP-L954V1S\Szymon <szostak1998@wp.pl>
2016-09-13 22:06:21 +0200

    Add button listener

--- core/src/com/mygdx/screens/GamePlayScreen.java
+++ core/src/com/mygdx/screens/GamePlayScreen.java
@@ -1,5 +1,9 @@
 package com.mygdx.screens;
 
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.entitles.Player;
 import com.mygdx.game.GraKlasa;
 
@@ -7,17 +11,41 @@ public class GamePlayScreen extends AbstractScreen
 {
 
 	private Player player;
+	private Button playerButton;
 
 	public GamePlayScreen(GraKlasa game)
 	{
 		super(game);
 		init();
+		initPlayerButton();
 
 	}
 
 	protected void init()
 	{
 		initPlayer();
+	}
+	
+	private void initPlayerButton()
+	{
+		playerButton = new Button(new ButtonStyle());
+		playerButton.setWidth(460);
+		playerButton.setHeight(360);
+		playerButton.setX(10);
+		playerButton.setY(170);
+		playerButton.setDebug(true);
+		
+		stage.addActor(playerButton);
+		
+		playerButton.addListener(new ClickListener(){
+			@Override
+			public boolean touchDown(InputEvent event, float x, float y, int pointer, int button)
+			{
+			System.err.println("klik?");
+				return super.touchDown(event, x, y, pointer, button);
+			}
+		});
+		
 	}
 
 	private void initPlayer()
